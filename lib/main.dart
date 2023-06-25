ec8465cb87592affe940c06822e1e7e2cc68d8a4
Charly <schwarz-fischer@hotmail.com>
2023-06-25 16:05:07 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:expenses_tracker/widgets/widgets.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    )
+  );
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:expenses_tracker/widgets/expenses.dart';
