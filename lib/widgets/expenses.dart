f624c782f972286cce73b1106589082471c58ea7
Charly <schwarz-fischer@hotmail.com>
2023-06-25 17:35:54 -0300

    clase 101

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:expenses_tracker/widgets/widgets.dart';
 import '../models/models.dart';
 
 class Expenses extends StatefulWidget {
@@ -14,15 +15,21 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
     Expense(title: 'Curso de Flutter', amount: 12.49, date: DateTime.now(), category: Category.educacion),
-    Expense(title: 'Cine', amount: 10.25, date: DateTime.now(), category: Category.ocio)
+    Expense(title: 'Cine', amount: 10.25, date: DateTime.now(), category: Category.ocio),
+    Expense(title: 'FCI', amount: 100000, date: DateTime.now(), category: Category.ahorro),
+    Expense(title: 'Viaje a Monaco', amount: 60000.20, date: DateTime.now(), category: Category.viajes),
+    Expense(title: 'Hamburgesa', amount: 9.77, date: DateTime.now(), category: Category.comida)
   ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('Aqui va los Charts'),
-          Text('Aqui va la lista de gastos')
+          const Text('Aqui va los Charts'),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses),
+          )
         ],
       ),
     );
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,2 @@
+export 'package:expenses_tracker/widgets/expenses_list.dart';
 export 'package:expenses_tracker/widgets/expenses.dart';
